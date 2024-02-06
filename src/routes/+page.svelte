94e5b4b772dc137df286966f8dd26e574a45ea82
kp2129 <kristapspolis0@gmail.com>
2024-02-06 09:45:33 +0200

    'playwright'

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Hello from "../lib/Hello.svelte";
+</script>
+
+<h1>Welcome to SvelteKit, Kristaps!</h1>
+<Hello name=Kristaps/>
+
+<style>
+    h1{
+        color: green;
+    }
+</style>
